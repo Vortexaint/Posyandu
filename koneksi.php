b49e2eaec4bbcd352a8d6b8539ad5554ef7c5d0f
Vortexaint <off19132@gmail.com>
2025-01-17 12:46:47 +0700

    Haiya cape

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
  
-$koneksi = mysqli_connect("localhost","rafi","R@fi8181.","redonion_posyandu");
+$koneksi = mysqli_connect("localhost","root","","posyandu");
 
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
